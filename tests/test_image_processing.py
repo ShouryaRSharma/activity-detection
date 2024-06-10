a8b8573dda48677a4ce876ef4bcceba403fd7469
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-06-10 14:45:52 +0100

    feat: :sparkles: Added MoonDreamImageProcessing (as shown in huggingface docs)

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -1,10 +1,32 @@
-import pytest
+from unittest.mock import patch
+import numpy as np
+from PIL import Image
 from activity_detection.image_processing import MoondreamImageProcessor
 
 
-@pytest.mark.xfail(reason="Not implemented yet")
-def test_process_frame():
-    processor = MoondreamImageProcessor()
-    frame = None  # Provide a sample frame for testing
-    detection_results = processor.process_frame(frame)
-    assert detection_results is not None
+@patch("transformers.AutoModelForCausalLM.from_pretrained")
+@patch("transformers.AutoTokenizer.from_pretrained")
+def test_process_frame(_, mock_model):
+    mock_frame = np.array(
+        [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]], dtype=np.uint8
+    )
+    mock_enc_image = np.array([1, 2, 3])
+    mock_model.return_value.encode_image.return_value = mock_enc_image
+    image_processor = MoondreamImageProcessor("model_id", "revision")
+
+    enc_image = image_processor.process_frame(mock_frame)
+
+    mock_model.return_value.encode_image.assert_called_once()
+    assert np.array_equal(enc_image, mock_enc_image)
+
+
+@patch("transformers.AutoModelForCausalLM.from_pretrained")
+@patch("transformers.AutoTokenizer.from_pretrained")
+def test_preprocess_frame(_, __):
+    mock_frame = np.array(
+        [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]], dtype=np.uint8
+    )
+    image_processor = MoondreamImageProcessor("model_id", "revision")
+    preprocessed_image = image_processor._preprocess_frame(mock_frame)
+    assert isinstance(preprocessed_image, Image.Image)
+    assert preprocessed_image.size == (2, 2)
