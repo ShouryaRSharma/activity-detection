7be8af920c12db9627c995ade964e1359bc3243c
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-06-10 16:42:15 +0100

    feat: :sparkles: Added activity detection through model

--- activity_detection/activity_detection.py
+++ activity_detection/activity_detection.py
@@ -1,13 +1,41 @@
 from abc import ABC, abstractmethod
 
+from PIL import Image
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
+
 
 class ActivityDetectionInterface(ABC):
     @abstractmethod
-    def detect_activity(self, detection_results):
+    def detect_activity(self, image: Image) -> bool:
         pass
 
 
-class SuspiciousActivityDetector(ActivityDetectionInterface):
-    def detect_activity(self, detection_results):
-        # TODO: Implement analyzing detection results to determine suspicious activity
-        pass
+class MoondreamActivityDetector(ActivityDetectionInterface):
+    def __init__(self, model_id: str, revision: str):
+        self.model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
+            model_id, trust_remote_code=True, revision=revision
+        )
+        self.tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(
+            model_id, revision=revision
+        )
+
+    def detect_activity(self, image: Image) -> bool:
+        """Detect if there is a person close to the camera in the input image.
+
+        Args:
+            image (Image): The input image to analyze
+
+        Returns:
+            bool: True if a person is close to the camera, False otherwise
+        """
+        prompt = "Is there a person close to the camera in this image? (Only answer 'YES' or 'NO')"
+        encoded_image = self.model.encode_image(image)
+        answer = self.model.answer_question(encoded_image, prompt, self.tokenizer)
+        if answer not in ["YES", "NO"]:
+            raise ValueError("Invalid answer from the model")
+        return answer == "YES"
