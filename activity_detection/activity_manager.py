376ba1b48af978e95a64a815b2fdb0185ceff4b6
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-06-11 17:46:16 +0100

    refactor: :recycle: Updated manager to load data from yaml file

--- activity_detection/activity_manager.py
+++ activity_detection/activity_manager.py
@@ -1,15 +1,44 @@
 import threading
 from queue import Queue, Full, Empty
-from typing import TypeVar
-from activity_detection.inputs.camera_input import CameraInputInterface
-from activity_detection.processing.image_processing import ImageProcessingInterface
+from activity_detection.inputs.camera_input import (
+    CameraInputInterface,
+    IPCamera,
+    LocalCamera,
+)
+from activity_detection.processing.image_processing import (
+    ImageProcessingInterface,
+    DefaultImageProcessor,
+)
 from activity_detection.classifiers.activity_classifier import (
     ActivityDetectionInterface,
+    MoondreamActivityDetector,
+    YOLOActivityDetector,
 )
 from activity_detection.logging_config import setup_logger
+from activity_detection.security.security_capture import DefaultVideoCapture
+from activity_detection.security.security_logging import DefaultSecurityLogging
 from activity_detection.security.security_module import SecurityModule
 
-T = TypeVar("T")
+
+COMPONENT_MAPPING: dict[str, dict[str | None]] = {
+    "camera_input": {
+        "IPCamera": IPCamera,
+        "LocalCamera": LocalCamera,
+    },
+    "image_processor": {
+        "DefaultImageProcessor": DefaultImageProcessor,
+    },
+    "activity_detector": {
+        "YOLOActivityDetector": YOLOActivityDetector,
+        "MoondreamActivityDetector": MoondreamActivityDetector,
+    },
+    "security_logging": {
+        "DefaultSecurityLogging": DefaultSecurityLogging,
+    },
+    "video_capture": {
+        "DefaultVideoCapture": DefaultVideoCapture,
+    },
+}
 
 
 class ActivityManager:
@@ -83,3 +112,51 @@ class ActivityManager:
             self.camera_input.stop_capture()
             self.security_module.video_capture.stop_video_capture()
             self.logger.info("Activity detection stopped.")
+
+    @classmethod
+    def from_config(cls, config: dict[str | None]):
+        camera_input_config = config["camera_input"]
+        camera_input_class = next(iter(camera_input_config))
+        camera_input_args = camera_input_config[camera_input_class].get("args", ())
+        camera_input = cls.create_object(
+            "camera_input", camera_input_class, *camera_input_args
+        )
+
+        image_processor_config = config["image_processor"]
+        image_processor_class = next(iter(image_processor_config))
+        image_processor = cls.create_object("image_processor", image_processor_class)
+
+        activity_detector_config = config["activity_detector"]
+        activity_detector_class = next(iter(activity_detector_config))
+        activity_detector_args = activity_detector_config[activity_detector_class].get(
+            "args", ()
+        )
+        activity_detector = cls.create_object(
+            "activity_detector", activity_detector_class, *activity_detector_args
+        )
+
+        security_logging_config = config["security_logging"]
+        security_logging_class = next(iter(security_logging_config))
+        security_logging = cls.create_object("security_logging", security_logging_class)
+
+        video_capture_config = config["video_capture"]
+        video_capture_class = next(iter(video_capture_config))
+        video_capture_args = video_capture_config[video_capture_class].get("args", ())
+        video_capture = cls.create_object(
+            "video_capture", video_capture_class, *video_capture_args
+        )
+
+        security_module = SecurityModule(video_capture, security_logging)
+
+        return cls(camera_input, image_processor, activity_detector, security_module)
+
+    @staticmethod
+    def create_object(component_type: str, class_name: str, *args, **kwargs):
+        if component_type not in COMPONENT_MAPPING:
+            raise ValueError(f"Invalid component type: {component_type}")
+
+        if class_name not in COMPONENT_MAPPING[component_type]:
+            raise ValueError(f"Invalid class name for {component_type}: {class_name}")
+
+        cls = COMPONENT_MAPPING[component_type][class_name]
+        return cls(*args, **kwargs)
--- activity_detection/main.py
+++ activity_detection/main.py
@@ -1,29 +1,13 @@
-from dotenv import load_dotenv
+import yaml
 
-import os
-
-from activity_detection.inputs.camera_input import IPCamera
-from activity_detection.processing.image_processing import DefaultImageProcessor
-from activity_detection.classifiers.activity_classifier import YOLOActivityDetector
-from activity_detection.security.security_capture import DefaultVideoCapture
-from activity_detection.security.security_logging import DefaultSecurityLogging
-from activity_detection.security.security_module import SecurityModule
 from activity_detection.activity_manager import ActivityManager
 
 
 def main():
-    load_dotenv()
-    ipv4_address = os.getenv("IPV4_ADDRESS")
-    camera_input = IPCamera(ipv4_address)
-    image_processor = DefaultImageProcessor()
-    activity_detector = YOLOActivityDetector()
-    security_logging = DefaultSecurityLogging()
-    video_capture = DefaultVideoCapture("suspicious_activity_videos")
-    security_module = SecurityModule(video_capture, security_logging)
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
 
-    activity_manager = ActivityManager(
-        camera_input, image_processor, activity_detector, security_module
-    )
+    activity_manager = ActivityManager.from_config(config)
     activity_manager.run_activity_detection()
 
 
