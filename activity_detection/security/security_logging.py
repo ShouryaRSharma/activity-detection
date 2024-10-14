5adb7d7cf99a952b0f575a55f278a460857ca3b8
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-10-14 12:05:23 +0100

    refactor: :recycle: Removed TTS for now as this will be server logs

--- activity_detection/security/security_logging.py
+++ activity_detection/security/security_logging.py
@@ -1,11 +1,12 @@
-import os
 from abc import ABC, abstractmethod
 
 from activity_detection.logging_config import setup_logger
-from gtts import gTTS
 
 
 class SecurityLoggingInterface(ABC):
+    def __init__(self):
+        self.logger = setup_logger(self.__class__.__name__)
+
     @abstractmethod
     def log_suspicious_activity(self):
         pass
@@ -16,20 +17,10 @@ class SecurityLoggingInterface(ABC):
 
 
 class DefaultSecurityLogging(SecurityLoggingInterface):
-    def __init__(self):
-        self.logger = setup_logger(self.__class__.__name__)
-
     def log_suspicious_activity(self):
         message = "Person detected!"
         self.logger.info(message)
-        self.say_log(message)
 
     def log_no_suspicious_activity(self):
         message = "No persons detected."
         self.logger.info(message)
-        self.say_log(message)
-
-    def say_log(self, message):
-        tts = gTTS(text=message, lang="en")
-        tts.save("log_message.mp3")
-        os.system("afplay log_message.mp3")
